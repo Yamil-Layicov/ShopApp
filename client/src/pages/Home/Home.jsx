6124eaf34d8250d1d4782fed88684898b95b7ac8
Yamil-Layicov <yamilbl@code.edu.az>
2023-07-30 21:57:16 +0400

    added card

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,3 +1,5 @@
+import Categories from "../../components/Categories/Categories"
+import Contact from "../../components/Contact/Contact"
 import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts"
 import Slider from "../../components/Slider/Slider"
 
@@ -7,6 +9,8 @@ const Home = () => {
       <Slider/>
       <FeaturedProducts type="featured"/>
       <FeaturedProducts type="trending"/>
+      <Categories/>
+      <Contact/>
     </div>
   )
 }
--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
 
 const Products = () => {
   return (
-    <div className='products'>Products</div>
+    <div className='products'>
+      <div className="left">
+        <div className="filterItem">
+          <h2>Product Categories</h2>
+          <div className="inputItem">
+            <input type="checkbox" id="1" value={1}/>
+            <label htmlFor="1">1</label>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Filter by Price</h2>
+        </div>
+        <div className="filterItem">
+          <h2>Sort by</h2>
+        </div>
+      </div>
+      <div className="right"></div>
+    </div>
   )
 }
 
